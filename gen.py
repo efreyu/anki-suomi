be70c1a1201ed0f8d74997a952b285ad0565b16d
Artur Barilo <efreyu@gmail.com>
2025-12-15 23:53:04 +0200

    master | update words cloze

--- gen.py
+++ gen.py
@@ -22,6 +22,7 @@ import generators.verbs as gen_verbs
 import generators.general as gen_general
 import generators.dialogs as gen_dialogs
 import generators.verbs_cloze as gen_cloze_verbs
+import generators.words_cloze as gen_cloze_words
 
 generators = [
     # {"name": "generate_verbs", "call": gen_verbs.gen_verbs, "expected": True, "args": ['db/general_verbs/verbs1.json', 'Finnish::Verbtypes::Verbtype1', 'Finnish_Verbs_1.apkg']},
@@ -31,13 +32,14 @@ generators = [
     # {"name": "generate_verbs", "call": gen_verbs.gen_verbs, "expected": True, "args": ['db/general_verbs/verbs5.json', 'Finnish::Verbtypes::Verbtype5', 'Finnish_Verbs_5.apkg']},
     # {"name": "generate_verbs", "call": gen_verbs.gen_verbs, "expected": True, "args": ['db/general_verbs/verbs6.json', 'Finnish::Verbtypes::Verbtype6', 'Finnish_Verbs_6.apkg']},
     # {"name": "generate_verbs", "call": gen_verbs.gen_verbs, "expected": True, "args": ['db/general_verbs/verbs7.json', 'Finnish::Verbtypes::Verbtype7', 'Finnish_Verbs_7.apkg']},
-    {"name": "numbers", "call": gen_general.gen_cards, "expected": True, "args": ['db/numbers/numbers1.json', 'Kirjakielinen numerot - normaali', 'Suomi::Numerot::Kirjakielinen-1::normaali', 'Suomi_Kirjakielinen_1n.apkg', 'regular', 445256733, 902348237123]},
-    {"name": "numbers", "call": gen_general.gen_cards, "expected": True, "args": ['db/numbers/numbers1.json', 'Kirjakielinen numerot - käänteinen', 'Suomi::Numerot::Kirjakielinen-1::käänteinen', 'Suomi_Kirjakielinen_1k.apkg', 'reverse', 445256734, 902348237223]},
+    # {"name": "numbers", "call": gen_general.gen_cards, "expected": True, "args": ['db/numbers/numbers1.json', 'Kirjakielinen numerot - normaali', 'Suomi::Numerot::Kirjakielinen-1::normaali', 'Suomi_Kirjakielinen_1n.apkg', 'regular', 445256733, 902348237123]},
+    # {"name": "numbers", "call": gen_general.gen_cards, "expected": True, "args": ['db/numbers/numbers1.json', 'Kirjakielinen numerot - käänteinen', 'Suomi::Numerot::Kirjakielinen-1::käänteinen', 'Suomi_Kirjakielinen_1k.apkg', 'reverse', 445256734, 902348237223]},
     # {"name": "dialogs", "call": gen_dialogs.gen_dialogs, "expected": True, "args": ['db/lessons/27-lesson/dialog1.json', 'Finnish::Lessons::Lesson27::Olga', 'Finnish_Lessons27_Olga.apkg']},
     # {"name": "dialogs", "call": gen_dialogs.gen_dialogs, "expected": True, "args": ['db/lessons/27-lesson/dialog2.json', 'Finnish::Lessons::Lesson27::Alex', 'Finnish_Lessons27_Alex.apkg']},
     # {"name": "dialogs", "call": gen_dialogs.gen_dialogs, "expected": True, "args": ['db/lessons/27-lesson/dialog3.json', 'Finnish::Lessons::Lesson27::Pedro', 'Finnish_Lessons27_Pedro.apkg']},
     # {"name": "cloze_verbs", "call": gen_cloze_verbs.gen_verbs_cloze, "expected": True, "args": [987654321, 'db/verbs_cloze/verbs1/*.json', 'Finnish::Verbtypes Cloze::Verbtype1', 'Finnish_Verbs_Cloze_1.apkg']},
-    {"name": "Sanasto", "call": gen_general.gen_cards, "expected": True, "args": ['db/vocabulary/vocabulary1.json', 'Sanasto 1', 'Suomi::Sanasto::Sanasto-1', 'Suomi_Sanasto_1.apkg', 'regular', 445256744, 902348237323]},
+    # {"name": "Sanasto", "call": gen_general.gen_cards, "expected": True, "args": ['db/vocabulary/vocabulary1.json', 'Sanasto 1', 'Suomi::Sanasto::Sanasto-1', 'Suomi_Sanasto_1.apkg', 'regular', 445256744, 902348237323]},
+    {"name": "cloze_verbs", "call": gen_cloze_words.gen_words_cloze, "expected": True, "args": [952344321, 'db/cloze_sets/adjektiivit.json', 'Finnish::Adjektiivit Cloze::Adjektiivit', 'Finnish_Words_Cloze_Adjektiivit.apkg']},
 ]
 
 def main():
---
+++ generators/words_cloze.py
@@ -0,0 +1,137 @@
+import genanki
+import json
+import os
+import sys
+import shutil
+import glob
+import generators.gen_utils as utils
+
+def gen_words_cloze(deck_id, json_dir, deck_name, apkg_filename='Finnish_Words.apkg'):
+
+    # Create the model for the Anki cards
+    model_id = 3414565112
+
+    front_html = utils.load_text('templates/verbs_cloze/front.html')
+    back_html = utils.load_text('templates/verbs_cloze/back.html')
+    css_text = utils.load_text('templates/verbs_cloze/styles.css')
+
+    model = genanki.Model(
+        model_id,
+        'Word Cloze with Image and Audio',
+        fields=[
+            {'name': 'Word'},
+            {'name': 'Image'},
+            {'name': 'Text'},
+            {'name': 'Translation'},
+            {'name': 'Node'},
+            {'name': 'KPT'},
+            {'name': 'Audio'},
+        ],
+        templates=[
+            {
+                'name': 'Word Conjugation Cloze Card',
+                'qfmt': front_html,
+                'afmt': back_html,
+            },
+        ],
+        css=css_text,
+        model_type=genanki.Model.CLOZE
+    )
+
+    # Create the decks
+    deck = genanki.Deck(
+        deck_id,
+        deck_name,
+    )
+
+    # Directory for media files (audio and images)
+    if os.path.exists('media'):
+        shutil.rmtree('media')
+    media_folder = 'media'
+    os.makedirs(media_folder, exist_ok=True)
+
+    # List to store paths to media files
+    media_files = []
+    words = []
+
+    json_files = glob.glob(json_dir)
+    skip_media = False
+
+    # skip_media = True
+    # json_files = json_files[:1]
+
+    for json_file in json_files:
+        print(f"Processing file: {json_file}")
+
+        # Load data from the JSON file
+        with open(json_file, 'r', encoding='utf-8') as f:
+            words = json.load(f)
+
+        # Add cards to the deck
+        for word_data in words:
+            word = word_data['word']
+            question = word
+            sanitized_image_filename = ""
+
+            # Handle optional image
+            if 'image' in word_data and not skip_media:
+                image_url = word_data['image']
+                if len(image_url):
+                    sanitized_image_filename = os.path.basename(image_url)
+                    image_path = os.path.join(media_folder, sanitized_image_filename)
+                    utils.download_image(image_url, image_path)
+                    media_files.append(image_path)
+
+            # Generate conjugations and audio
+            text = word_data['text']
+            translation = word_data['translation']
+            keys = word_data['keys']
+            result_parts = []
+            audio_parts = ""
+            for i, (txt, key) in enumerate(zip(text, keys), start=1):
+                audio_parts += txt.replace("{}", key) + " "
+                # cloze = f"{{{{c{i}::{key}}}}}"
+                cloze = f"{{{{c1::{key}}}}}"
+                result_parts.append(txt.format(cloze))
+
+            result = "<br/>".join(result_parts)
+            sanitized_audio_filename = ""
+            if not skip_media:
+                sanitized_audio_filename = utils.sanitize_filename(f"{word}_{utils.short_hash(audio_parts, 8)}.mp3")
+                audio_path = os.path.join(media_folder, sanitized_audio_filename)
+                utils.generate_audio(audio_parts.strip(), audio_path)
+                media_files.append(audio_path)
+
+            note_field = ""
+            if 'note' in word_data:
+                note_field = word_data['note']
+
+            kpt_field = ""
+            # if 'KPT' in word_data:
+            #     kpt_field = word_data['KPT']
+            # Add note
+            note = genanki.Note(
+                model=model,
+                fields=[
+                    question,
+                    f'<img src="{sanitized_image_filename}" style="max-width:200px; height:auto;"/>' if len(sanitized_image_filename) > 0 else "",   # Image
+                    result,
+                    translation,
+                    note_field,
+                    kpt_field,
+                    f"[sound:{sanitized_audio_filename}]" if len(sanitized_audio_filename) > 0 else ""
+                ],
+                guid=utils.short_hash(audio_parts, 8)  # Unique identifier based on audio content
+            )
+            deck.add_note(note)
+
+    # Create the package and include media files
+    package = genanki.Package(deck)
+    package.media_files = media_files  # Add media files (audio + images) to the package
+    package.write_to_file(apkg_filename)
+
+    return True
+
+
+if __name__ == '__main__':
+    sys.exit(1)
